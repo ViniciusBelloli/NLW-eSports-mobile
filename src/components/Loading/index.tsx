e623bd8a9e2ae4b8897ba27062e9e8181f2edfe5
Vinicius Belloli <vbelloli@outlook.com>
2022-09-16 11:44:58 +0100

    Project Finished

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import { ActivityIndicator } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { THEME } from '../../theme';
+
+import { styles } from './styles';
+
+export function Loading() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ActivityIndicator color={THEME.COLORS.PRIMARY} />
+    </SafeAreaView>
+  );
+}
